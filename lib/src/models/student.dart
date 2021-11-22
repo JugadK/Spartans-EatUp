324c71f743ce1706f4feabee65a42d99237bf74e
Jugad Khajuria <curaww@gmail.com>
2021-11-22 12:39:22 -0800

    [add] student db model

--- lib/src/models/student.dart
+++ lib/src/models/student.dart
@@ -3,20 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:spartans_eatup/main.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:spartans_eatup/src/models/order.dart';
+import 'package:spartans_eatup/src/models/order_list.dart';
 
-@immutable
 class Student {
-  final String email;
-  final String uid;
+  String email;
+  List<Order> orders;
 
-  Student({
-    required this.email,
-    required this.uid,
-  });
+  Student({required this.email, required this.orders}) {
+    if (email.substring(email.length - 9, email.length) != "@sjsu.edu") {
+      throw ("Email is not an sjsu email");
+    }
+  }
 
-  Student.fromJson(Map<String, Object?> json)
-      : this(
-          email: json['genre']! as String,
-          uid: json['likes']! as String,
-        );
+  Student fromJson(Map<String, Object?> json) {
+    orders = json['orders']! as List<Order>;
+    email = json['email']! as String;
+
+    return Student(email: email, orders: orders);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'email': email, 'orders': orders};
+  }
 }
