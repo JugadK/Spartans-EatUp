d3b2257188741e5b9031b9ab3fcf4af9519ea3d4
Jugad Khajuria <curaww@gmail.com>
2021-11-22 12:39:00 -0800

    [add] order db model

--- lib/src/models/order.dart
+++ lib/src/models/order.dart
@@ -1,26 +1,27 @@
-import 'package:spartans_eatup/main.dart';
 import 'package:flutter/material.dart';
-import 'package:spartans_eatup/main.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-@immutable
 class Order {
-  final String name;
+  String name;
 
   //REMEMBER : all prices for items need to be stored in integers, cpus suck
-  // at figuring out base 10 decimals :( , we have to later on
+  // at figuring out base 10 decimals :( , we have to later on convert it back
+  // to a decimal
 
-  final int price;
+  int price;
 
   Order({
     required this.name,
     required this.price,
   });
 
-  Order.fromJson(Map<String, Object?> json)
-      : this(
-          name: json['name']! as String,
-          price: json['price']! as int,
-        );
+  Order fromJson(Map<String, Object?> json) {
+    name = json['name']! as String;
+    price = json['price']! as int;
+
+    return Order(name: name, price: price);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'name': name, 'price': price};
+  }
 }
