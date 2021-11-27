613b79bb82d38e10f2cbca431dee04bca21899ac
Jugad Khajuria <curaww@gmail.com>
2021-11-26 19:25:11 -0800

    [fix] fromJson method for student

--- lib/src/models/student.dart
+++ lib/src/models/student.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:spartans_eatup/src/models/order.dart';
 import 'package:spartans_eatup/src/models/order_list.dart';
+import 'package:decimal/decimal.dart';
 
 class Student {
   String email;
@@ -16,12 +17,22 @@ class Student {
     }
   }
 
-  Student fromJson(Map<String, Object?> json) {
+  factory Student.fromJson(Map<String, dynamic> json) {
+    var ordersJson = json['orders'] as List;
+    List<Order> orders =
+        ordersJson.map((orderJson) => Order.fromJson(orderJson)).toList();
+    //List<Order> orders = json['orders'];
+    String email = json['email'];
+
+    return Student(email: email, orders: orders);
+  }
+
+  /*Student fromJson(Map<String, Object?> json) {
     orders = json['orders']! as List<Order>;
     email = json['email']! as String;
 
     return Student(email: email, orders: orders);
-  }
+  } */
 
   Map<String, dynamic> toJson() {
     return {'email': email, 'orders': orders};
