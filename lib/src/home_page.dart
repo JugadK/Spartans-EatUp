808cd92031a760a592795035909a2aa39608e824
Jugad Khajuria <curaww@gmail.com>
2021-11-26 19:24:52 -0800

    [add] test restaurant page

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,16 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:spartans_eatup/main.dart';
-
+import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:spartans_eatup/main.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:spartans_eatup/src/models/order.dart';
+import 'package:spartans_eatup/src/models/student.dart';
+import 'package:spartans_eatup/src/restaurant_pages/test_restaurant.dart';
 
 class HomePage extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue[900],
-      child: const Text("Idk put something here"),
-    );
+        child: GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => TestRestaurant().build(context)));
+      },
+      child: Text("Go to Test Restaurant"),
+    ));
   }
 }
