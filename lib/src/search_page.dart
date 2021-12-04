7314676eabd54b48e11a26e17b768e9e6b078a20
Jugad Khajuria <curaww@gmail.com>
2021-12-04 03:14:55 -0800

    [add] search bar displays all retaurants, have to add query next

--- lib/src/search_page.dart
+++ lib/src/search_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 import 'package:spartans_eatup/src/constants.dart';
 import 'colors.dart' as color;
 
@@ -11,6 +15,10 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  CollectionReference restaurants =
+      FirebaseFirestore.instance.collection('restaurants');
+
+  List<String> restaurantNames = [];
   List<Widget> itemsData = [];
 
   void getPostsData() {
@@ -49,36 +57,92 @@ class _SearchPageState extends State<SearchPage> {
       );
     });
     setState(() {
+      print(itemsData);
       itemsData = listItems;
+    });
+  }
+
+  void getListData() async {
+    QuerySnapshot snapshot = await restaurants.get();
+    List<String> queryNames = [];
+
+    snapshot.docs.forEach((element) {
+      queryNames.add(element.get("name"));
+    });
+
+    setState(() {
+      restaurantNames = queryNames;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: color.AppColor.white,
-        body: Container(
-          padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                "Search",
-                style: TextStyle(
-                  fontSize: 40,
-                  color: Color(0xffF6B92E),
-                  fontWeight: FontWeight.w800,
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              CupertinoSearchTextField(),
-            ],
+      // This is handled by the search bar itself.
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          buildFloatingSearchBar(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildFloatingSearchBar() {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return FloatingSearchBar(
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {},
+      // Specify a custom transition to be used for
+      // animating between opened and closed stated.
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.food_bank),
+            onPressed: () {},
           ),
-        ));
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        getListData();
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.blueAccent,
+            elevation: 4.0,
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: restaurantNames.map((e) {
+                  return Container(
+                    height: 60,
+                    color: Colors.blueAccent,
+                    child: Text(e),
+                  );
+                }).toList()
+
+                /* children: Colors.accents.map((color) {
+                return Container(height: 112, color: color);
+              }).toList(), */
+                ),
+          ),
+        );
+      },
+    );
   }
 }
