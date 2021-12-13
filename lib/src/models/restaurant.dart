f70334fd29f09444c566407dfa7297441b429412
Jugad Khajuria <curaww@gmail.com>
2021-12-12 17:40:32 -0800

    [add] menu list to restaurant model

--- lib/src/models/restaurant.dart
+++ lib/src/models/restaurant.dart
@@ -6,22 +6,28 @@ class Restaurant {
   String email;
   String name;
   List<String> currentOrderUsers;
+  List<Order> menu;
 
   Restaurant({
     required this.email,
     required this.name,
     required this.currentOrderUsers,
+    required this.menu,
   });
 
   factory Restaurant.fromJson(Map<String, dynamic?> json) {
     String name = json['name']! as String;
     String email = json['email']! as String;
     List<String> currentOrderUsers = List.from(json['currentOrderUsers']);
+    var menuJson = json['menu']! as List;
+    List<Order> menu =
+        menuJson.map((menuJson) => Order.fromJson(menuJson)).toList();
 
     return Restaurant(
       email: email,
       name: name,
       currentOrderUsers: currentOrderUsers,
+      menu: menu,
     );
   }
 
