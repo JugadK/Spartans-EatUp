c4886d84fca2cdc33d0ae8227fa3914c0991d2be
Jugad Khajuria <curaww@gmail.com>
2021-12-13 16:58:22 -0800

    [add] picture value for storing the link where an orders picture is

--- lib/src/models/order.dart
+++ lib/src/models/order.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:decimal/decimal.dart';
 import 'package:spartans_eatup/src/models/restaurant.dart';
@@ -13,17 +15,26 @@ class Order {
 
   String restaurant;
 
-  Order({required this.name, required this.price, required this.restaurant});
+  String picture;
 
+  Order(
+      {required this.name,
+      required this.price,
+      required this.restaurant,
+      required this.picture});
+
+  @override
   String toString() {
-    return name + " " + price.toString();
+    return name + " \$" + price.toString();
   }
 
   Order.fromJson(Map<String, dynamic> json)
       : this(
-            name: json['name']! as String,
-            price: Decimal.parse(json['price'].toString()),
-            restaurant: json['restaurant']! as String);
+          name: json['name']! as String,
+          price: Decimal.parse(json['price'].toString()),
+          restaurant: json['restaurant']! as String,
+          picture: json['picture']! as String,
+        );
 
   Map<String, dynamic> toJson() =>
       {'name': name, 'price': price.toJson(), 'restaurant': restaurant};
