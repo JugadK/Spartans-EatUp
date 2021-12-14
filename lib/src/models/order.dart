3914c3a47ddf9d949ecbd80e2c3e3218cb45dabc
Jugad Khajuria <curaww@gmail.com>
2021-12-14 12:32:38 -0800

    [add] fielkds to check if order is on the menu

--- lib/src/models/order.dart
+++ lib/src/models/order.dart
@@ -7,9 +7,9 @@ import 'package:spartans_eatup/src/models/restaurant.dart';
 class Order {
   String name;
 
-  //REMEMBER : all prices for items need to be stored in integers, cpus suck
-  // at figuring out base 10 decimals :( , we have to later on convert it back
-  // to a decimal
+  //REMINDER: computers suck at decimal math, we will use this decimal class
+  // instead for dealing with money
+  // https://pub.dev/packages/decimal
 
   Decimal price;
 
@@ -17,25 +17,43 @@ class Order {
 
   String picture;
 
-  Order(
-      {required this.name,
-      required this.price,
-      required this.restaurant,
-      required this.picture});
+  // onCurrentMenu purely matters when we ask the user is looking at the
+  // the menu, so only in the menu array located in the restaurant model
+  // does it have any purpose, it can be ignored in other parts
+
+  bool onCurrentMenu;
+
+  List<dynamic> tags = [];
+
+  Order({
+    required this.name,
+    required this.price,
+    required this.restaurant,
+    required this.picture,
+    required this.onCurrentMenu,
+    required this.tags,
+  });
 
   @override
   String toString() {
     return name + " \$" + price.toString();
   }
 
-  Order.fromJson(Map<String, dynamic> json)
+  Order.fromJson(Map<String, dynamic?> json)
       : this(
-          name: json['name']! as String,
-          price: Decimal.parse(json['price'].toString()),
-          restaurant: json['restaurant']! as String,
-          picture: json['picture']! as String,
-        );
+            name: json['name']! as String,
+            price: Decimal.parse(json['price'].toString()),
+            restaurant: json['restaurant']! as String,
+            picture: json['picture']! as String,
+            onCurrentMenu: json['onCurrentMenu']! as bool,
+            tags: json['tags']! as List<dynamic>);
 
-  Map<String, dynamic> toJson() =>
-      {'name': name, 'price': price.toJson(), 'restaurant': restaurant};
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'price': price.toJson(),
+        'restaurant': restaurant,
+        'picture': picture,
+        'onCurrentMenu': onCurrentMenu,
+        'tags': tags,
+      };
 }
